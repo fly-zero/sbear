53209d341dcaf8a2191ea4f4ccef1c4a6fc33245
flyzero <fly.zero@foxmail.com>
2023-11-30 22:49:18 +0800

    feat: 添加 socket 和 signal 的 IO 处理逻辑

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-#include <cstddef>
+#include <fcntl.h>
 #include <limits.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <sys/select.h>
+#include <sys/signalfd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include <cassert>
+#include <cstddef>
 #include <cstdlib>
 #include <cstring>
 
@@ -91,7 +99,96 @@ inline const char * get_make_cc_cxx_args(const char * arg0) {
     return buff;
 }
 
-static void do_make(const char * prog, int argc, char * argv[]) {
+inline int listen_unix_socket(const char * path) {
+    // 创建 unix domain socket
+    auto const fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    strcpy(addr.sun_path, path);
+
+    // 绑定 unix domain socket
+    if (bind(fd, reinterpret_cast<struct sockaddr *>(&addr), sizeof addr) != 0) {
+        perror("bind");
+        goto cleanup;
+    }
+
+    // 设置 unix domain socket 的权限
+    if (chmod(path, 0600) != 0) {
+        perror("chmod");
+        goto cleanup;
+    }
+
+    // 设置非阻塞模式
+    if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK) != 0) {
+        perror("fcntl");
+        goto cleanup;
+    }
+
+    // 设置 CLOSE_ON_EXEC
+    if (fcntl(fd, F_SETFD, fcntl(fd, F_GETFD) | FD_CLOEXEC) != 0) {
+        perror("fcntl");
+        goto cleanup;
+    }
+
+    return fd;
+
+cleanup:
+    if (fd >= 0) {
+        close(fd);
+    }
+
+    return -1;
+}
+
+inline int create_signalfd(int signo) {
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, signo);
+    int const sigfd = signalfd(-1, &sigset, SFD_NONBLOCK | SFD_CLOEXEC);
+    if (sigfd < 0) {
+        perror("signalfd");
+        return -1;
+    }
+
+    return sigfd;
+}
+
+inline void read_unix_socket(int sock) {
+    while (true) {
+        char buff[8192];
+        auto const n = recvfrom(sock, buff, sizeof buff, 0, nullptr, nullptr);
+        if (n > 0) {
+            std::cout << "recv: " << buff << std::endl;
+            // TODO: 解析命令
+        } else if (n == 0) {
+            break;
+        } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            break;
+        } else {
+            perror("recvfrom");
+            break;
+        }
+    }
+}
+
+inline void read_signal_fd(int sigfd) {
+    struct signalfd_siginfo siginfo;
+    auto const n = read(sigfd, &siginfo, sizeof siginfo);
+    if (n == sizeof siginfo) {
+        std::cout << "recv signal: " << siginfo.ssi_signo << std::endl;
+    } else if (n == 0) {
+        std::cout << "signal fd closed" << std::endl;
+    } else {
+        perror("read");
+    }
+}
+
+inline void do_make(const char * prog, int argc, char * argv[]) {
     auto const cmdline = static_cast<char **>(alloca((argc + 2) * sizeof(char *)));
     for (int j = 0; j < argc; ++j) {
         cmdline[j] = argv[j];
@@ -110,13 +207,45 @@ static void do_make(const char * prog, int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // 监听 unix domain socket
+    int const sock = listen_unix_socket(socket_path);
+    if (sock < 0) {
+        return;
+    }
+
+    // 为 SIGCHLD 创建描述符
+    int const sigfd = create_signalfd(SIGCHLD);
+    if (sigfd < 0) {
+        return;
+    }
+
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(sock, &readfds);
+    FD_SET(sigfd, &readfds);
+    auto const maxfdp1 = std::max(sock, sigfd) + 1;
+
     auto const pid = fork();
     if (pid == 0) {
         execvp("make", cmdline);
-    } else {
-        // TODO: 监听 unix domain socket
-
-        // TODO: 等待 make 进程退出
+    } else while (true) {
+        fd_set tmp = readfds;
+        auto const n = select(maxfdp1, &tmp, nullptr, nullptr, nullptr);
+        if (n > 0) {
+            if (FD_ISSET(sock, &tmp)) {
+                read_unix_socket(sock);
+            } else if (FD_ISSET(sigfd, &tmp)) {
+                read_signal_fd(sigfd);
+                break;
+            } else {
+                assert(false);
+            }
+        } else if (n == 0) {
+            continue;
+        } else {
+            perror("select");
+            break;
+        }
     }
 }
 
